01881bcb6031085aa1544b96ef493860b6467e13
Tabitht <imafidontabitha@gmail.com>
2024-11-29 19:00:22 -0800

    made corrections to the header

--- event-management/src/Layouts/Header.jsx
+++ event-management/src/Layouts/Header.jsx
@@ -7,9 +7,9 @@ import NavBar from "../components/NavBar/NavBar";
 const Header = () => {
     return (
         <div className="flex justify-center">
-            <div className="items-center w-screen md:h-[104px] bg-[#FFFFFF] border-[1px] border-[#806B77] flex flex-row justify-between">
+            <div className="items-center w-screen md:h-[104px] bg-[#FFFFFF] border-[1px] px-[40px] border-[#806B77] flex flex-row justify-between">
                 <div className="md:hidden"><NavBar /></div>
-                <div className="md:text-[2rem] font-bold text-[#412234] font-pattaya md:ml-[40px] xl:ml-[80px]">Tickety</div>
+                <div className="md:text-[2rem] font-bold text-[#412234] font-pattaya md:ml-[40px]">Tickety</div>
                 <div className="hidden md:text-[16px] md:text-[#806B77] md:font-raleway md:flex md:flex-row md:justify-center md:gap-[24px] xl:gap-[48px]">
                     <p className="text-[#1B0E16] font-bold">Home</p>
                     <p>About Us</p>
