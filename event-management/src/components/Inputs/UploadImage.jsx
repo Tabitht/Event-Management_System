c61312c3ef37cbc52ec9a4535d36cad1c249cef1
Tabitht <imafidontabitha@gmail.com>
2025-04-04 13:44:40 +0100

    made some changes

--- event-management/src/components/Inputs/UploadImage.jsx
+++ event-management/src/components/Inputs/UploadImage.jsx
@@ -1,16 +1,22 @@
-import uploadIcon from "../../assets/UploadImage.svg";
+import uploadIcon from "../../assets/uploadImage.svg";
 
 const UploadImage = () => {
-    return (
-        <>
-            <div className="bg-white w-[50%] md:w-[164px] h-[124px] rounded-[4px] border-[2px] border-[#806B77] p-[1.25rem]">
-                <div className="flex flex-col items-center text-center gap-[1rem]">
-                <img className="w-[40%]" src={uploadIcon} alt="uploading image icon" />
-                <p className="font-raleway text-[0.75rem] md:-[1rem] text-[#412234]">Upload Photos and Video</p>
-                </div>
-            </div>
-        </>
-    )
-}
+  return (
+    <>
+      <div className="bg-white w-[50%] md:w-[164px] h-[124px] rounded-[4px] border-[2px] border-[#806B77] p-[1.25rem]">
+        <div className="flex flex-col items-center text-center gap-[1rem]">
+          <img
+            className="w-[40%]"
+            src={uploadIcon}
+            alt="uploading image icon"
+          />
+          <p className="font-raleway text-[0.75rem] md:-[1rem] text-[#412234]">
+            Upload Photos and Video
+          </p>
+        </div>
+      </div>
+    </>
+  );
+};
 
 export default UploadImage;
